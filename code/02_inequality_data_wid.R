96cff00cfa74032122efb9c19d9494ed7a5f0901
Mariana Miyazima <mariana_miyazima@usp.br>
2026-02-25 17:51:36 -0300

    Update 02_inequality_data_wid.R

--- code/02_inequality_data_wid.R
+++ code/02_inequality_data_wid.R
@@ -28,14 +28,14 @@ data_incshare_pretax <- download_wid(
     "p99.9p100",    # Top 0.1%
     "p99.99p100",   # Top 0.01%
     "p0p10"),       # Bottom 10%
-  metadata = TRUE, ages = 999, pop = "j") #all ages// pop- equally split between adults
+  metadata = TRUE, ages = 992, pop = "j") #all ages// pop- equally split between adults
 
 data_incshare_pretax <- data.table(data_incshare_pretax)
 data_incshare_pretax <- data_incshare_pretax[nchar(country) == 2] ## country-codes bigger than 2 digits are world or country regions
 data_incshare_pretax <- unique(data_incshare_pretax) # remove duplicate data
 data_incshare_pretax <- dcast(data_incshare_pretax, country + countryname + year + percentile + shortname  ~ variable, value.var = "value")
 data_incshare_pretax <- data_incshare_pretax[, percentile := paste0("sptinc_", percentile)] 
-data_incshare_pretax <- dcast(data_incshare_pretax, country + countryname + year + shortname ~ percentile, value.var = "sptinc999j")
+data_incshare_pretax <- dcast(data_incshare_pretax, country + countryname + year + shortname ~ percentile, value.var = "sptinc992j")
 
 
 
@@ -53,14 +53,14 @@ data_incshare_dispinc <- download_wid(
     "p99.9p100",    # Top 0.1%
     "p99.99p100",   # Top 0.01%
     "p0p10"),       # Bottom 10%
-  metadata = TRUE, ages = 999, pop = "j") #all ages// pop- equally split between adults
+  metadata = TRUE, ages = 992, pop = "j") # ages 20+ (adults, including elderly)// pop- equally split between adults
 
 data_incshare_dispinc <- data.table(data_incshare_dispinc)
 data_incshare_dispinc <- data_incshare_dispinc[nchar(country) == 2] ## country-codes bigger than 2 digits are world or country regions
 data_incshare_dispinc <- unique(data_incshare_dispinc) # remove duplicate data
 data_incshare_dispinc <- dcast(data_incshare_dispinc, country + countryname + year + percentile + shortname ~ variable, value.var = "value")
 data_incshare_dispinc <- data_incshare_dispinc[, percentile := paste0("sdinc_", percentile)] 
-data_incshare_dispinc <- dcast(data_incshare_dispinc, country + countryname + year +shortname ~ percentile, value.var = "sdiinc999j")
+data_incshare_dispinc <- dcast(data_incshare_dispinc, country + countryname + year +shortname ~ percentile, value.var = "sdiinc992j")
 
 
 
@@ -79,7 +79,7 @@ gini_pt <- download_wid(
   areas = "all",              
   years = 1960:2024,        
   perc = "p0p100",
-  ages = 999,               #all ages
+  ages = 992,               #20+ ages
   metadata = TRUE, 
   pop = "j")                #equally split between adults
 
@@ -94,13 +94,13 @@ gini_di <- download_wid(
   areas = "all",              
   years = 1960:2024,        
   perc = "p0p100",
-  ages = 999,               #all ages
+  ages = 992,               # ages 20+ (including elderly)
   metadata = TRUE, 
-  pop = "j")                #equally split between adults
+  pop = "j")                # equally split between adults
 
 gini_di <- data.table(gini_di)
 gini_di <- gini_di[nchar(country)==2]
-# barplot(table(gini_di$year)) ## só a partir de 1980
+#barplot(table(gini_di$year)) ## só a partir de 1980
 # length(unique(gini_di$country))
 gini_di <- dcast(gini_di, country + countryname + year ~ variable, value.var = "value")
 
@@ -111,7 +111,7 @@ fwrite(gini_pt, "gini_pretaxincome-wdi.csv")
 
 ineq <- merge(income, gini_pt, by = c("country", "countryname", "year"))
 ineq <- merge(ineq, gini_di, by = c("country", "countryname", "year"), all.x = T)
-setnames(ineq, old = c("gdiinc999j", "gptinc999j"),
+setnames(ineq, old = c("gdiinc992j", "gptinc992j"),
                new = c("gini_dinc", "gini_ptinc"))
 
 fwrite(ineq, "inequality_data-wid.csv")
