887fc509b16e71b01d4ae4c15cfc0d1bd2f46d65
RayneAS <raynealvesantos@gmail.com>
2026-03-10 23:18:35 -0300

    Clean code and gen new results
    Add version of event study with less legs and leds

--- code/04_did_estimate_part2.R
+++ code/04_did_estimate_part2.R
@@ -68,8 +68,7 @@ panel[is.na(Reform.Dummy), Reform.Dummy := 0L]
 panel[is.na(Significant.Reform), Significant.Reform := 0L]
 stopifnot("Reform.Dummy" %in% names(panel))
 
-# 2 - Define auxiliary vars to did package ------------------------------------------------------
-#Treatment is omega intensity
+# 2 - Define auxiliary vars to did package -------------------------------------
 
 setorder(panel, Code, year)
 
@@ -80,13 +79,6 @@ panel[, first_treat_year :=
       else NA_integer_,
       by = Code]
 
-# Define pre-period flag
-panel[, pre_period := 0L]
-panel[treated_group == 1 & !is.na(first_treat_year) & 
-        year < first_treat_year, pre_period := 1L]
-panel[treated_group == 0, pre_period := 1L]
-
-
 #Define country numeric id did package
 panel[, id := .GRP, by = Code]
 
@@ -94,7 +86,24 @@ panel[, id := .GRP, by = Code]
 panel[, gvar := first_treat_year]
 panel[is.na(gvar), gvar := 0]
 
-#data checks
+#Define dose as level at adoption
+dose_dt <- panel[gvar > 0 & year == gvar, .(dose = -Omega), by = Code]
+panel <- merge(panel, dose_dt, by = "Code", all.x = TRUE)
+
+#Define dose=0 for never-treated
+panel[gvar == 0, dose := 0]
+
+#dose=0 for treated with Omega missing at adoption (Rubolino-imputed NA)
+panel[gvar > 0 & is.na(dose), dose := 0]
+
+#Define continuous adoption: treated only if dose>0
+panel[, gvar_cont := fifelse(dose > 0, gvar, 0L)]
+
+panel[gvar_cont == 0, dose := 0]
+
+# 2.1. checks--------------------------------------------------------------------
+
+#check1
 panel[, .(
   n_units = uniqueN(id),
   n_treated = uniqueN(id[gvar > 0]),
@@ -105,7 +114,7 @@ panel[, uniqueN(gvar)]
 unique_g_var <- sort(unique(panel$gvar))
 unique_g_var
 
-#check g_var
+#check2
 check_gvar <- panel[gvar > 0,
                     .(
                       gvar_unique = unique(gvar),
@@ -116,77 +125,34 @@ check_gvar <- panel[gvar > 0,
 
 check_gvar[gvar_unique != min_year_treated]
 
-
-# dose as level at adoption
-dose_dt <- panel[gvar > 0 & year == gvar, .(dose = -Omega), by = Code]
-panel <- merge(panel, dose_dt, by = "Code", all.x = TRUE)
-
-#dose=0 for never-treated
-panel[gvar == 0, dose := 0]
-
-#dose=0 for treated with Omega missing at adoption (Rubolino-imputed NA)
-panel[gvar > 0 & is.na(dose), dose := 0]
-
-#continuous adoption: treated only if dose>0
-panel[, gvar_cont := fifelse(dose > 0, gvar, 0L)]
-
-
-panel[gvar_cont == 0, dose := 0]
-
+#check3
 # sanity checks required by contdid (time-invariant)
 stopifnot(panel[, all(uniqueN(dose) == 1L), by = Code]$V1 |> all())
 stopifnot(panel[, all(uniqueN(gvar_cont) == 1L), by = Code]$V1 |> all())
 
-# who switches to never-treated in continuous definition
+#check4
+#who switches to never-treated in continuous definition
 panel[gvar > 0 & gvar_cont == 0,
       .(Country=unique(Country), gvar=unique(gvar), dose=unique(dose)),
       by=Code][]
 
-
-# 1) dose constante por país
+#check5
+#dose constant by country
 panel[, .(uniq_dose = uniqueN(dose)), by = Code][uniq_dose != 1]
 
-# 2) gvar constante por país
+#check6
+#gvar constant by country
 panel[, .(uniq_gvar = uniqueN(gvar)), by = Code][uniq_gvar != 1]
 
-# 3) never-treated: gvar==0 e dose time-invariant (0 ou NA, mas constante)
+#never-treated: gvar==0 e dose time-invariant (0 ou NA, mas constante)
 panel[gvar == 0, .(uniq_dose = uniqueN(dose)), by = Code][uniq_dose != 1]
 
 
 panel[gvar > 0 & is.na(dose), .N, by = .(Code, Country)][order(-N)]
 
 
-# manter apenas variáveis necessárias
-vars_need <- c("Code", "year", "pt_share_top1", "dose", "gvar_cont")
-dt_cc <- panel[complete.cases(panel[, ..vars_need])]
-
-res_cont <- cont_did(
-  yname = "pt_share_top1",
-  dname = "dose",
-  gname = "gvar_cont",
-  tname = "year",
-  idname = "Code",
-  xformula = ~1,
-  data = dt_cc,
-  target_parameter = "level",
-  aggregation = "eventstudy",
-  treatment_type = "continuous",
-  control_group = "notyettreated",
-  biters = 100,
-  cband = TRUE,
-  num_knots = 0,
-  degree = 1,
-)
-
-summary(res_cont)
-
-ggcont_did(res_cont)
-
-
-panel <- merge(panel, dose_dt, by = "Code", all.x = TRUE)
-panel[gvar == 0, dose := 0]   # never-treated com dose 0
-
-View(panel[,list(Country,Code, year, Reform.Dummy, gvar, dose, Omega)])
+#check7
+#View(panel[,list(Country,Code, year, Reform.Dummy, gvar, dose, Omega)])
 
 panel[gvar > 0 & is.na(dose), .N]
 panel[gvar == 0 & is.na(dose), .N]
@@ -202,13 +168,122 @@ table(is.na(panel$tax_revenue_frac))
 table(is.na(panel$gross_savings_frac))
 
 
+# 3- DID continuous estimated with contdid package ---------------------------------
+
+#Treatment is omega intensity 
+
+vars_need <- c("Code", "year", "pt_share_top1", "dose", "gvar_cont")
+dt_cc <- panel[complete.cases(panel[, ..vars_need])]
+
+
+res_cont <- cont_did(
+  yname = "pt_share_top1",
+  dname = "dose",
+  gname = "gvar_cont",
+  tname = "year",
+  idname = "Code",
+  xformula = ~1,
+  data = dt_cc,
+  target_parameter = "level",
+  aggregation = "eventstudy",
+  treatment_type = "continuous",
+  control_group = "notyettreated",
+  biters = 999,
+  cband = TRUE,
+  num_knots = 2,
+  degree = 1,
+)
+
+summary(res_cont)
+
+ggcont_did(res_cont)
+
+#checks
+names(res_cont)
+names(res_cont$event_study)
+str(res_cont$event_study, max.level = 1)
+
+#extract estimated values
+es <- data.table(
+  e   = res_cont$event_study$egt,
+  att = res_cont$event_study$att.egt,
+  se  = res_cont$event_study$se.egt
+)
+
+crit <- unname(res_cont$event_study$crit.val.egt)
+
+es[, `:=`(
+  lo = att - crit * se,
+  hi = att + crit * se
+)]
+
+es_win <- es[e >= -5 & e <= 10]
+
+#plot figure with selected legs and leads
+
+png(file.path(figure_dir, "event_study_dose_share_income1_contdid.png"),
+    width = 1600, height = 1000, res = 200)
+
+ggplot(es_win, aes(x = e, y = att)) +
+  geom_ribbon(aes(ymin = lo, ymax = hi),
+              fill = "gray80", alpha = 0.4) +
+  geom_hline(yintercept = 0, linetype = "dashed", linewidth = 0.5, color = "gray40") +
+  geom_vline(xintercept = -0.5, linetype = "dashed", linewidth = 0.5, color = "gray40") +
+  geom_line(linewidth = 0.8, color = "black") +
+  geom_point(size = 2, color = "black") +
+  scale_x_continuous(breaks = seq(-5, 10, by = 1)) +
+  labs(
+    x = "Event time",
+    y = "ATT"
+  ) +
+  theme_classic(base_size = 13) +
+  theme(
+    axis.title = element_text(face = "bold"),
+    axis.text = element_text(color = "black")
+  )
+
+dev.off()
+
+#table
+tab_es <- es_win[, .(
+  e,
+  coef = round(att, 4),
+  se   = round(se, 4),
+  lo   = round(lo, 4),
+  hi   = round(hi, 4)
+)]
+
+tab_es
+
+
+#test
+pre_idx <- which(es$e >= -5 & es$e <= -1)
+beta <- es$att[pre_idx]
+
+IFmat <- res_cont$event_study$inf.function$dynamic.inf.func.e
+
+V <- cov(IFmat[, pre_idx, drop = FALSE])
+
+W <- as.numeric(t(beta) %*% solve(V) %*% beta)
+df <- length(beta)
+pval <- 1 - pchisq(W, df)
+
+W
+pval
+
+
+# 4 - TWFE event-study with continuous treatment intensity controls ------------------
+
+#Treatment is omega intensity 
+
+#part2: Treatment is omega intensity and we estimate with 
+
 
 panel[gvar > 0, e := year - gvar]
 panel[gvar == 0, e := 0L] # never-treated não tem event time
 
-# manter janela
+#selected leds and lags
 dt_es <- panel[(gvar == 0) | (e >= -5 & e <= 10)]
-
 
 nrow(dt_es[complete.cases(
   pt_share_top1,
@@ -219,13 +294,10 @@ nrow(dt_es[complete.cases(
   working_age_pop
 )])
 
-
 panel[gvar > 0 & is.na(dose), .N]
 
-# View(panel[,list(Country,year, Reform.Dummy, Significant.Reform, gvar, dose, 
-# Omega, first_treat_year, pre_period, post_adoption_zero, notyet, e)])
 
-### TYPE1
+
 #ESTIMATION
 # Interações: i(e, dose, ref=-1) cria dummies de e interagidas com dose, 
 #omitindo e=-1
